de1462484824a428def9164f42665a044c539188
Sarma Tangirala <vtangira@buffalo.edu>
2013-06-21 17:47:43 -0400

    Adding a log utility. Problem with streams.

--- utils.cpp
+++ utils.cpp
@@ -1,16 +1,32 @@
-#include <stdio.h>
+#include <ios>
+#include <iostream>
+#include <string.h>
+#include <sstream> // for stringbuf
 
 #include "utils.h"
 
-
-//extern state_t DEBUG;
+state_t Log::DEBUG = OFF;
+std::ios *Log::stream = nullptr;
 
 void Log::log_msg(char *msg) {
 
-  //state_t DEBUG;
-
   if (DEBUG == ON) {
-    printf ("LOG_MSG: %s\n", msg);
+    //stream << "LOG_MSG: " << msg << std::endl;
+    stream << "LOG_MSG: ";
+  }
+}
+
+Log::Log(state_t state, char *stream) {
+
+  if (DEBUG != ON) {
+    DEBUG = state;
+  }
+
+  if (Log::stream != nullptr) {
+    if (strcmp(stream, "cout") == 0) {
+      std::stringbuf *buffer = new std::stringbuf();
+      Log::stream = new std::iostream(buffer);
+    }
   }
 }
 
--- utils.h
+++ utils.h
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <ios>
 
 #pragma once
 
@@ -14,9 +14,20 @@ class Log {
 
   private:
           static state_t DEBUG;
+          static std::ios *stream;
 
   public:
           void log_msg(char *msg);
-          Log(state_t state) { DEBUG = state; }
-          Log() { DEBUG = OFF; }
+
+          Log(state_t state, char *stream);
+
+          // This version when you need it.
+          //Log(state_t state, ios *stream);
+
+          Log(){}
+
+          ~Log() {
+            delete stream;
+          }
+
 };
